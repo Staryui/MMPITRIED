bfc278e5b324bc72bffcc4fea5dd4022f44203f4
Staryui <42847225+Staryui@users.noreply.github.com>
2018-10-08 23:55:00 -0400

    Adding an Image

--- Memgo/sketch.js
+++ Memgo/sketch.js
@@ -1,8 +1,15 @@
 //MEMGO V 0
 
+
+function preload() {
+	img = loadImage("Kermit.jpeg");}
+
 function setup() {
 	createCanvas(640, 640);
+	text("I should do HomeWork, Also me watch YouTube", 20, 20);
 }
-
+var img;
 function draw() {
 	background(28);
+	image(img, 0, 0);
+}
