6b1abc64822e49dc87f75207d8830a3e6d74bd9a
guywithpen <abreujaysonschool@gmail,com>
2018-12-04 15:19:48 -0500

    Me trying to FInal the best WAY I can

---
+++ MMP210 FINAL/sketch.js
@@ -0,0 +1,15 @@
+//MEMGO V 0
+
+
+function preload() {
+	img = loadImage("Kermit.jpeg");}
+
+function setup() {
+	createCanvas(940, 940);
+	//text("I should do HomeWork, Also me watch YouTube", 20, 20);
+}
+var img;
+function draw() {
+	background(28);
+	image(img, 0, 0);
+}
